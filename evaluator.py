6e891329afd1d299083ff181bf87416b1f210773
Qi Ouyang <qo234@b-19-7.c.hpc-slurm-9c75.internal>
2024-12-13 19:36:32 -0500

    only feed in init states

--- evaluator.py
+++ evaluator.py
@@ -21,7 +21,7 @@ from normalizer import Normalizer
 class ProbingConfig(ConfigBase):
     probe_targets: str = "locations"
     # lr: float = 0.0002
-    lr: float = 0.02
+    lr: float = 0.005
     epochs: int = 20
     schedule: LRSchedule = LRSchedule.Cosine
     sample_timesteps: int = 30
@@ -113,7 +113,8 @@ class ProbingEvaluator:
             for batch in tqdm(dataset, desc="Probe prediction step"):
                 ################################################################################
                 # TODO: Forward pass through your model
-                pred_encs = model(states=batch.states, actions=batch.actions)
+                init_states = batch.states[:, 0:1]  # BS, 1, C, H, W
+                pred_encs = model(states=init_states, actions=batch.actions)
                 pred_encs = pred_encs.transpose(0, 1)  # # BS, T, D --> T, BS, D
 
                 # Make sure pred_encs has shape (T, BS, D) at this point
@@ -210,7 +211,8 @@ class ProbingEvaluator:
         for idx, batch in enumerate(tqdm(val_ds, desc="Eval probe pred")):
             ################################################################################
             # TODO: Forward pass through your model
-            pred_encs = model(states=batch.states, actions=batch.actions)
+            init_states = batch.states[:, 0:1]  # BS, 1 C, H, W
+            pred_encs = model(states=init_states, actions=batch.actions)
             # # BS, T, D --> T, BS, D
             pred_encs = pred_encs.transpose(0, 1)
 
